8691f7a9238e6eb36735cf8866f74df9660f3c3f
Mhir Alaedine <mhiralaedine@gmail.com>
2024-10-14 13:45:37 +0100

    Create general.js

---
+++ general.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    var container = document.querySelector('.container');
+    var mobileMenu = document.getElementById('mobileMenu');
+    container.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
+}
+  
+    window.addEventListener('resize', function() {
+    var container = document.querySelector('.container');
+    var mobileMenu = document.getElementById('mobileMenu');
+    
+    if (window.innerWidth > 990) {
+      if (container.classList.contains('open')) {
+        container.classList.remove('open');
+        mobileMenu.classList.remove('open');
+      }
+    }
+});
