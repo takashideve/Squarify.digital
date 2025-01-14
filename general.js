7c5f00baba69047e0b3a22190e3e3479bbbc7eca
Takashi-the-dev <mhiralaedine@gmail.com>
2025-01-14 17:15:20 +0100

    Update general.js

--- general.js
+++ general.js
@@ -20,3 +20,55 @@ function toggleMenu() {
 window.onload = function() {
   document.getElementById("year").textContent = new Date().getFullYear();
 };
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const progressValue = document.querySelector(".progress-value");
+
+  const observer = new IntersectionObserver(
+      (entries) => {
+          entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                  progressValue.style.width = "99%";
+              }
+          });
+      },
+      {
+          threshold: 0.5,
+      }
+  );
+
+  const progressBar = document.querySelector(".progress");
+  observer.observe(progressBar);
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const portfolioItems = document.querySelectorAll(".roadmap_item");
+
+  portfolioItems.forEach((item) => {
+      item.style.opacity = 0;
+      item.style.transition = "opacity 0.5s ease";
+  });
+
+  const observer = new IntersectionObserver(
+      (entries) => {
+          entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                  triggerPortfolioItems();
+              }
+          });
+      },
+      { threshold: 0.5 }
+  );
+
+  const progressBar = document.querySelector(".progress");
+  observer.observe(progressBar);
+
+  function triggerPortfolioItems() {
+      setTimeout(() => (portfolioItems[0].style.opacity = 1), 500);
+      setTimeout(() => (portfolioItems[1].style.opacity = 1), 1200);
+      setTimeout(() => (portfolioItems[2].style.opacity = 1), 2000);
+      setTimeout(() => (portfolioItems[3].style.opacity = 1), 3800);
+  }
+});
